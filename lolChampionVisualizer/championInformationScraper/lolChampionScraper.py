d537283aa13c3eb52c8089b8f0b059abb76607c2
Yuriy <Sorlondo@Gmail.com>
2020-05-04 22:29:11 -0400

    Handled more champion statistics
    - Have taken care of more of the outlier champions so there are fewer
    - Have streamlined it a bit more so that things do not need to be hard coded for better future iterations

--- lolChampionVisualizer/championInformationScraper/lolChampionScraper.py
+++ lolChampionVisualizer/championInformationScraper/lolChampionScraper.py
@@ -1,4 +1,8 @@
 from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import json
 
 '''
@@ -35,59 +39,93 @@ def getAllChampions():
 # method used to get the statistics of each champion found in the Champions.txt file
 def getChampionStatistics(championList):
     championStatistics = {}
-    print(championList)
+    lastValidChampion = ""
+    dataSortChampionValue = ""
     # initializes our web driver
     driver = webdriver.Chrome()
+
+    #championList = ["ashe", "aurelion sol", "akali"]
     for championName in championList:
         championName = championName.replace("\n", "")
-        driver.get("https://leagueoflegends.fandom.com/wiki/"+championName.replace(" ", "_"))
-        statisticsContainer = driver.find_elements_by_xpath("/html/body/div[3]/section/div[2]/article/div[1]/div[2]/div[2]/div[4]/div[2]/div[1]/aside[1]/*")
-        
-        # if this is the incorrect container then get the other one
-        if len(statisticsContainer) <= 0 or statisticsContainer[0].tag_name != "h2" or statisticsContainer[0].find_element_by_xpath('a').text != "Base statistics":
-            statisticsContainer = driver.find_elements_by_xpath("/html/body/div[3]/section/div[2]/article/div[1]/div[1]/div[2]/div[3]/div[2]/div[1]/aside[1]/*")
         championStatistics[championName] = {}
-        print(championName)
-        for stat in statisticsContainer[1:-1]:
-            for container in stat.find_elements_by_xpath('*'):
-                if container.tag_name != "section":
+        try:
+            try:
+                driver.get("https://leagueoflegends.fandom.com/wiki/"+championName.replace(" ", "_"))
+                WebDriverWait(driver, 6).until(EC.visibility_of_element_located((By.ID, "infobox-champion-container")))
+            except TimeoutException:
+                try:
+                    driver.get("https://leagueoflegends.fandom.com/wiki/List_of_champions")
+                    championElements = driver.find_elements_by_xpath("/html/body/div[3]/section/div[2]/article/div[1]/div[1]/div[2]/table[2]/tbody/*")
+                    foundChampion = False
+                    for element in championElements:
+                        # if we have found the champion then get its name from the attribute and go to the site
+                        if foundChampion:
+                            hrefLink = element.find_element_by_xpath('td[1]/span/span[2]/a').get_attribute("href")
+                            dataSortChampionValue = element.find_elements_by_xpath("*")[0].get_attribute("data-sort-value").lower()
+                            driver.get(hrefLink)
+                            break
+                        # if the champion who is above the searched for champion is found then go to the next champion
+                        sortValue = element.find_elements_by_xpath("*")[0].get_attribute("data-sort-value")
+                        if sortValue is not None and sortValue.lower() == lastValidChampion:
+                            foundChampion = True
+                            continue
+
+                    WebDriverWait(driver, 6).until(EC.visibility_of_element_located((By.ID, "infobox-champion-container")))
+                except TimeoutException:
                     continue
-                first_object = container.find_element_by_xpath('section/div[1]')
-                second_object = container.find_element_by_xpath('section/div[2]')
-                first_label = "N/A"
-                first_value = "N/A"
-                second_label = "N/A"
-                second_value = "N/A"
-                label_options = ['div/a[2]', 'div/a', 'div/span/a', 'div/span/a[2]']
-                
-                for option in label_options:
-                    try:
-                        if first_label == "N/A" or first_label == "":
-                            first_label = first_object.find_element_by_xpath(option).text
-                    except:
+
+            statisticsContainer = driver.find_elements_by_xpath("/html/body/div[3]/section/div[2]/article/div[1]/div[2]/div[2]/div[4]/div[2]/div[1]/aside[1]/*")
+
+            # if this is the incorrect container then get the other one
+            if len(statisticsContainer) <= 0 or statisticsContainer[0].tag_name != "h2" or statisticsContainer[0].find_element_by_xpath('a').text != "Base statistics":
+                    statisticsContainer = driver.find_elements_by_xpath("/html/body/div[3]/section/div[2]/article/div[1]/div[1]/div[2]/div[3]/div[2]/div[1]/aside[1]/*")
+
+            if len(statisticsContainer) <= 0 or statisticsContainer[0].tag_name != "h2" or statisticsContainer[0].find_element_by_xpath('a').text != "Base statistics":
+                    statisticsContainer = driver.find_elements_by_xpath("/html/body/div[3]/section/div[2]/article/div[1]/div[2]/div[2]/div[3]/div[2]/div[1]/aside[1]/*")
+
+            # go through all of the stats that are listed for the champion but bypass the first and last one since therye not necessary
+            for stat in statisticsContainer[1:-1]:
+                for container in stat.find_elements_by_xpath('*'):
+                    if container.tag_name != "section":
                         continue
-                
-                for option in label_options:
-                    try:
-                        if second_label == "N/A" or second_label == "":
-                            second_label = second_object.find_element_by_xpath(option).text
-                    except:
-                        continue
-                
-                first_value =  (container.find_elements_by_xpath('section/div[1]')[-1].text).replace(first_label, "")
-                second_value =  container.find_elements_by_xpath('section/div[2]')[-1].text.replace(second_label, "")
+                    first_object = container.find_element_by_xpath('section/div[1]')
+                    second_object = container.find_element_by_xpath('section/div[2]')
+                    first_label = "N/A"
+                    second_label = "N/A"
+                    label_options = ['div/a[2]', 'div/a', 'div/span/a', 'div/span/a[2]']
+                    # gets the first label of the statistic based on the label options
+                    for option in label_options:
+                        try:
+                            if first_label == "N/A" or first_label == "":
+                                first_label = first_object.find_element_by_xpath(option).text
+                        except:
+                            continue
+
+                    # gets the second label of the statistic based on the label options
+                    for option in label_options:
+                        try:
+                            if second_label == "N/A" or second_label == "":
+                                second_label = second_object.find_element_by_xpath(option).text
+                        except:
+                            continue
+
+                    # gets the values of the labels
+                    first_value = container.find_elements_by_xpath('section/div[1]')[-1].text.replace(first_label, "")
+                    second_value = container.find_elements_by_xpath('section/div[2]')[-1].text.replace(second_label, "")
 
 
-                first_label = first_label.replace("\n", "")
-                first_value = first_value.replace("\n", "")
-                second_label = second_label.replace("\n", "")
-                second_value = second_value.replace("\n", "")
-                championStatistics[championName][first_label] = first_value
-                championStatistics[championName][second_label] = second_value
-
-                if championName.lower() == "caitlyn":
-                    driver.quit()
-                    return championStatistics
+                    first_label = first_label.replace("\n", "")
+                    first_value = first_value.replace("\n", "")
+                    second_label = second_label.replace("\n", "")
+                    second_value = second_value.replace("\n", "")
+                    championStatistics[championName][first_label] = first_value
+                    championStatistics[championName][second_label] = second_value
+                    lastValidChampion = championName
+                    if dataSortChampionValue != "":
+                        lastValidChampion = dataSortChampionValue
+                        dataSortChampionValue = ""
+        except:
+            continue
 
     driver.quit()
     return championStatistics
